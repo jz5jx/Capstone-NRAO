0be6f04a7a70f33ca7332fb0d298f54ee9bfa228
pkbondalapati <pavan.bond.kumar@gmail.com>
2021-01-27 22:41:11 -0500

    Initial commit for unit test

---
+++ Capstone-NRAO/module/pbga_test.py
@@ -0,0 +1,11 @@
+import unittest
+import numpy as np
+from astropy.io import fits
+from matplotlib import pyplot as plt
+from pbga import PBGA
+
+
+class PBGATester(unittest.TestCase):
+
+    def setUp(self):
+        pass
