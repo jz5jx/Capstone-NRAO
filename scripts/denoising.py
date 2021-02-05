b0d57d5562b813b0f120c42099e7375164fd19c8
spaylor4 <spaylor4@yahoo.com>
2021-02-05 10:46:41 -0500

    Update to not change in place

--- scripts/denoising.py
+++ scripts/denoising.py
@@ -4,6 +4,10 @@ def circle_crop(fits_image, rad_factor = 1.0):
     rad = fits_image.imdata.shape[0]/2
     rad_sq = rad**2
 
-    for ix,iy in np.ndindex(fits_image.imdata.shape):
+    new_imdata = fits_image.imdata.copy()
+
+    for ix,iy in np.ndindex(new_imdata.shape):
         if (ix - rad)**2 + (iy - rad)**2 > rad_sq:
-            fits1.imdata[ix, iy] = np.nan
+            new_imdata[ix, iy] = np.nan
+
+    return new_imdata
